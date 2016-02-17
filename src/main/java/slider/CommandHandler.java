29a382af02246d3707c9b3297c651eae3bc2136f
Taichi Uragami <backpaper0@gmail.com>
2016-02-17 22:06:58 +0900

    画面の状態を手元でも確認できるようにしたい
    既知の問題
    
    * スクリーンショットを撮るタイミングが早すぎる
    * 画像を表示するときの縦横比率がおかしい

--- src/main/java/slider/CommandHandler.java
+++ src/main/java/slider/CommandHandler.java
@@ -1,8 +1,17 @@
 package slider;
 
+import java.awt.DisplayMode;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 
+import javax.imageio.ImageIO;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
@@ -12,8 +21,9 @@ public class CommandHandler {
 
     @OnMessage
     public void post(String command, Session session) throws Exception {
+        String[] commands = command.split(",");
         Robot robot = new Robot();
-        switch (command) {
+        switch (commands[0]) {
         case "LEFT":
             robot.keyPress(KeyEvent.VK_LEFT);
             break;
@@ -21,5 +31,21 @@ public class CommandHandler {
             robot.keyPress(KeyEvent.VK_RIGHT);
             break;
         }
+        DisplayMode dm = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+                .getDisplayMode();
+        Rectangle r = new Rectangle(0, 0, dm.getWidth(), dm.getHeight());
+        BufferedImage src = robot.createScreenCapture(r);
+        int width = Integer.parseInt(commands[1]);
+        int height = width * src.getHeight() / src.getWidth();
+        BufferedImage dest = new BufferedImage(width, height, src.getType());
+        Graphics2D g = dest.createGraphics();
+        Image img = src.getScaledInstance(width, height, BufferedImage.SCALE_FAST);
+        g.drawImage(img, 0, 0, width, height, null);
+        g.dispose();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ImageIO.write(dest, "jpeg", out);
+        String data = "data:image/jpeg;base64,"
+                + Base64.getEncoder().encodeToString(out.toByteArray());
+        session.getAsyncRemote().sendText(data);
     }
 }
