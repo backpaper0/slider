12a64749c325b0902375a8d4e4913e4c8cf05b34
Taichi Uragami <backpaper0@gmail.com>
2016-06-14 22:11:18 +0900

    Basic認証をつけた

--- src/main/java/slider/Slider.java
+++ src/main/java/slider/Slider.java
@@ -1,8 +1,29 @@
 package slider;
 
+import java.security.Principal;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.api.AuthenticationMode;
+import io.undertow.security.handlers.AuthenticationCallHandler;
+import io.undertow.security.handlers.AuthenticationConstraintHandler;
+import io.undertow.security.handlers.AuthenticationMechanismsHandler;
+import io.undertow.security.handlers.SecurityInitialHandler;
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.idm.PasswordCredential;
+import io.undertow.security.impl.BasicAuthenticationMechanism;
 import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.Servlets;
@@ -17,7 +38,27 @@ public class Slider {
 
         PathHandler path = Handlers.path();
 
-        Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0").setHandler(path)
+        int[] cs = IntStream.concat(IntStream.rangeClosed('0', '9'), IntStream.rangeClosed('a', 'z')
+                .flatMap(c -> IntStream.of(c, Character.toUpperCase(c)))).toArray();
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        char[] password = new char[8];
+        for (int i = 0; i < password.length; i++) {
+            password[i] = (char) cs[sr.nextInt(cs.length)];
+        }
+        System.out.println(password);
+
+        IdentityManager identityManager = new MapIdentityManager(password);
+
+        HttpHandler handler = path;
+        handler = new AuthenticationCallHandler(handler);
+        handler = new AuthenticationConstraintHandler(handler);
+        List<AuthenticationMechanism> mechanisms = Collections
+                .singletonList(new BasicAuthenticationMechanism("Slider Realm"));
+        handler = new AuthenticationMechanismsHandler(handler, mechanisms);
+        handler = new SecurityInitialHandler(AuthenticationMode.PRO_ACTIVE, identityManager,
+                handler);
+
+        Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0").setHandler(handler)
                 .build();
         server.start();
 
@@ -35,5 +76,47 @@ public class Slider {
         DeploymentManager manager = container.addDeployment(builder);
         manager.deploy();
         path.addPrefixPath("/", manager.start());
+    }
+
+    private static class MapIdentityManager implements IdentityManager {
+
+        private final char[] password;
+
+        public MapIdentityManager(char[] password) {
+            this.password = password;
+        }
+
+        @Override
+        public Account verify(Account account) {
+            return account;
+        }
+
+        @Override
+        public Account verify(String id, Credential credential) {
+            if (Objects.equals(id, "slider") && credential instanceof PasswordCredential) {
+                if (Arrays.equals(password, ((PasswordCredential) credential).getPassword())) {
+                    return new Account() {
+
+                        private final Principal principal = () -> id;
+
+                        @Override
+                        public Set<String> getRoles() {
+                            return Collections.emptySet();
+                        }
+
+                        @Override
+                        public Principal getPrincipal() {
+                            return principal;
+                        }
+                    };
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public Account verify(Credential credential) {
+            return null;
+        }
     }
 }
